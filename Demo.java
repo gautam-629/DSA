334fc630837d463b861ffbcbc82ce7d25e06e110
gautam-629 <gautam629@gmail.com>
2023-08-16 12:10:05 +0545

    minor change

--- Demo.java
+++ Demo.java
@@ -1,16 +1,13 @@
 class Demo{
-    static void convertBinToDec(int n){
-        int decNum=0;
-        int pow=0;
-          while(n!=0){
-            int lastNum=n%10;
-            decNum+=(lastNum*Math.pow(2,pow));
-            pow++;
-            n/=10;
-          }
-          System.out.println(decNum);
-    }
+
     public static void main(String[] args) {
-        convertBinToDec(1010);
+        int numbers[]={2,4,6,8,10};
+
+        for(int i=0;i<numbers.length;i++){
+          for(int j=i+1;j<numbers.length;j++){
+            System.out.print("("+numbers[i] + "," + numbers[j] + ") ");
+          }
+          System.out.println();
+        }
     }
 }
